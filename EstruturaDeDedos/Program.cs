12b3b9a6a8c75c77532588c1abb6b789d71e7dc3
Luisa Fiamengui <luisalopesfiamengui@gmail.com>
2024-08-21 14:53:51 -0300

    Finalizando atividade Lista de Frutas

--- EstruturaDeDedos/Program.cs
+++ EstruturaDeDedos/Program.cs
@@ -1,10 +1,65 @@
-﻿internal class Program
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Minha primeira branch");
-        Console.WriteLine("Trabalhando com Git\\Github");
-        Console.WriteLine("Mais uma tentativa de trabalhar com Git");
+        //Console.WriteLine("Minha primeira branch");
+        //Console.WriteLine("Trabalhando com Git\\Github");
+        //Console.WriteLine("Mais uma tentativa de trabalhar com Git");
 
+        List<string> frutas = new List<string>();
+
+        frutas.Add("Morango");
+        frutas.Add("Manga");
+        frutas.Add("Goiaba");
+        frutas.Add("Uva");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Minha lista de frutas:");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+
+        }
+
+        Console.WriteLine("Fruta na indice 2: " + frutas[2]);
+
+        frutas.Insert(1, "Maracujá");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Minha lista de frutas:");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+
+        }
+
+        frutas[4] = "Pera";
+
+        Console.WriteLine();
+
+        Console.WriteLine("Minha lista de frutas:");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+
+        }
+
+        frutas.RemoveAt(3);
+        frutas.Remove("Morango");
+
+        Console.WriteLine();
+
+        Console.WriteLine("Minha lista de frutas:");
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+
+        }
+
+        frutas.Clear();
     }
 }
